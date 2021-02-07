72254b00edab62fdaa43c1c032d0196da8878d9e
Bruno Sousa <brunoh.henriqueh01@gmail.com>
2021-02-07 11:43:52 -0300

    Added new routes on App

--- mobile/src/routes.tsx
+++ mobile/src/routes.tsx
@@ -3,8 +3,13 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
+// Busca pelo orfanatos
 import OrphanagesMap from './screens/OrphanagesMap';
 import OrphanageDetails from './screens/OrphanageDetails';
+
+// Cadastro dos orfanatos
+import SelectMapPosition from './screens/createOrphanage/SelectMapPosition';
+import OrphanageData from './screens/createOrphanage/OrphanageData';
 
 const { Navigator, Screen } = createStackNavigator();
 
@@ -14,6 +19,9 @@ export default function Routes() {
       <Navigator screenOptions={{ headerShown: false }}>
         <Screen name="OrphanagesMap" component={OrphanagesMap}/>
         <Screen name="OrphanageDetails" component={OrphanageDetails}/>
+
+        <Screen name="SelectMapPosition" component={SelectMapPosition}/>
+        <Screen name="OrphanageData" component={OrphanageData}/>
       </Navigator>
     </NavigationContainer>
   );
